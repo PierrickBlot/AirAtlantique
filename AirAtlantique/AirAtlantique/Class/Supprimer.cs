f3e840d58017cfc8aeaa672d0713ae28c3adad3d
Pierrick <pierrickblot243@gmail.com>
2018-11-29 11:35:03 +0100

    supprimer

--- AirAtlantique/AirAtlantique/Class/Supprimer.cs
+++ AirAtlantique/AirAtlantique/Class/Supprimer.cs
@@ -10,8 +10,18 @@ namespace AirAtlantique.Class
     public class Supprimer
     {
         int id=1;
-        Connexion cnx = new Connexion();
-        MySqlCommand delete = new MySqlCommand("DELETE FROM avion WHERE numero =");
+        Connexion cnx = new Connexion();
 
-    }
+
+        MySqlCommand delete = new MySqlCommand("DELETE FROM avion WHERE numero =1");
+        connexion.Open();
+     MySqlDataReader reader = requete.ExecuteReader();
+        if (reader.HasRows)
+        {
+        while (reader.Read())
+        {
+        //recupération des données…
+        }
+        }
 }
+
